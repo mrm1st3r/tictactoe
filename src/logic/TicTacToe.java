ad39d50018bdd4b7318ccedbbd52888e5c1a5e14
Lukas <ltaake@fh-bielefeld.de>
2013-04-30 00:47:02 +0200

    Versionsnummer angepasst

--- src/logic/TicTacToe.java
+++ src/logic/TicTacToe.java
@@ -7,7 +7,7 @@ import exception.PlayerException;
 public class TicTacToe {
 	
 	public static final String NAME = "TicTacToe";
-	public static final String VERSION = "1.3";
+	public static final String VERSION = "1.4";
 
 	private UserInterface ui;
 	private PlayingField f;
