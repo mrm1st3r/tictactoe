67be0f95370d95ba1f7982cd4109ad087f8102ea
Lukas <lukas.taake@gmail.com>
2016-07-01 14:22:46 +0200

    added tests for LinearStrategy

---
+++ src/test/groovy/com/github/mrm1st3r/ttt/logic/strategy/LinearStrategyTest.groovy
@@ -0,0 +1,47 @@
+package com.github.mrm1st3r.ttt.logic.strategy
+
+import com.github.mrm1st3r.ttt.model.Coordinates
+import com.github.mrm1st3r.ttt.model.FieldSetException
+import com.github.mrm1st3r.ttt.model.PlayingField
+import spock.lang.Specification
+
+/**
+ *
+ *
+ * @author Lukas Taake
+ */
+class LinearStrategyTest extends Specification {
+
+    PlayingField playingField
+    List<Character> symbols
+    Strategy strategy
+
+    def setup() {
+        symbols = new ArrayList<Character>()
+        symbols.add((char)'X')
+        symbols.add((char)'O')
+        strategy = new LinearStrategy()
+
+        playingField = new PlayingField(PlayingField.DEFAULT_WIDTH, PlayingField.DEFAULT_HEIGHT, symbols)
+    }
+
+    def "should pick first field"() {
+        when:
+        def pos = strategy.calculateMove(playingField, symbols.get(0))
+
+        then:
+        pos == new Coordinates(1,1)
+    }
+
+    def "should throw exception when field is full"() {
+        given:
+        Map<Coordinates, Character> fields = playingField.fieldMap
+
+        when:
+        fields.replaceAll{Coordinates k, Character v -> return (Character)'X'}
+        strategy.calculateMove(playingField,symbols.get(0))
+
+        then:
+        thrown(FieldSetException)
+    }
+}
